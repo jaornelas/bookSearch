e11662419a1e981364aacd01a26545099f227809
jaornelas <ornelalf001@gmail.com>
2025-04-13 22:15:33 -0500

    add apolloServer to server.ts file

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,7 +1,14 @@
 import express from 'express';
 import path from 'node:path';
+import type { Request, Response } from 'express';
+
 import db from './config/connection.js';
-import routes from './routes/index.js';
+
+import { ApolloServer } from '@apollo/server'
+import { expressMiddleware } from '@apollo/server/express4';
+import { authenticateToken } from './services/auth.js';
+import { typeDefs, resolvers } from './schemas/index.js';
+
 
 // add these lines to make __dirname work
 import { fileURLToPath } from 'url';
@@ -13,22 +20,45 @@ const __dirname = dirname(__filename);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  console.log(__dirname);
-  app.use(express.static(path.join(__dirname, '../../client/dist')));
+const startApolloServer = async () => {
+  await server.start();
+  await db;
+
+  app.use(express.urlencoded({ extended: false }));
+  app.use(express.json());
+
+  app.use('/graphql', expressMiddleware(server as any,
+    {
+      context: authenticateToken as any
+    }
+  ));
+
+  // if we're in production, serve client/build as static assets
+  if (process.env.NODE_ENV === 'production') {
+    console.log(__dirname);
+    app.use(express.static(path.join(__dirname, '../../client/dist')));
+
+    app.get('*', (_req: Request, res: Response) => {
+      res.sendFile(path.join(__dirname, '../../client/dist/index.html'));
+    });
+  }
+
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+    console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
+  });
+
 }
 
-// app.get('*', (_req, res) => {
-//   res.sendFile(path.join(__dirname, '../../client/dist', 'index.html'));
-// });
+startApolloServer();
 
-app.use(routes);
+//app.use(routes);
 
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-});
