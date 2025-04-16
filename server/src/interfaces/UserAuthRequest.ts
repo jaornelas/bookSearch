36400eee653e7b12d5eff297d575c0b4502c715d
jaornelas <ornelalf001@gmail.com>
2025-04-16 11:33:04 -0500

    remove extension from UserAuthRequest file

--- server/src/interfaces/UserAuthRequest.ts
+++ server/src/interfaces/UserAuthRequest.ts
@@ -1,5 +1,5 @@
 import type { Request } from 'express';
-import type IJwtPayload from './JwtPayload.js';
+import type IJwtPayload from './JwtPayload';
 
 export default interface IUserAuthRequest extends Request { 
   user: IJwtPayload
