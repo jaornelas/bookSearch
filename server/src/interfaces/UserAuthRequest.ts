ab2eaa2d7c3cc9fbc6806c7036fc933f6b547d4d
jaornelas <ornelalf001@gmail.com>
2025-04-18 12:04:24 -0500

    add file extension

--- server/src/interfaces/UserAuthRequest.ts
+++ server/src/interfaces/UserAuthRequest.ts
@@ -1,5 +1,5 @@
 import type { Request } from 'express';
-import type IJwtPayload from './JwtPayload';
+import type IJwtPayload from './JwtPayload.js';
 
 export default interface IUserAuthRequest extends Request { 
   user: IJwtPayload
