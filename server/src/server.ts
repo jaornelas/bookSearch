52db3bbaf3f9fd8a66b6915b8c821a9ce33d6113
jaornelas <ornelalf001@gmail.com>
2025-04-18 12:18:59 -0500

    delete jwtauth

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,29 +1,36 @@
 import express from 'express';
 import path from 'node:path';
 import type { Request, Response } from 'express';
-import db from './config/connection.js'
-import { ApolloServer } from '@apollo/server';// Note: Import from @apollo/server-express
+
+import db from './config/connection.js';
+
+import { ApolloServer } from '@apollo/server'
 import { expressMiddleware } from '@apollo/server/express4';
-import { typeDefs, resolvers } from './schemas/index.js';
 import { authenticateToken } from './services/auth-services.js';
+import { typeDefs, resolvers } from './schemas/index.js';
+
+
+// add these lines to make __dirname work
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 
-
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
+// END - add these lines to make __dirname work
 
+const app = express();
+const PORT = process.env.PORT || 3001;
 const server = new ApolloServer({
   typeDefs,
-  resolvers
+  resolvers,
 });
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 const startApolloServer = async () => {
   await server.start();
-  await db();
-
-  const PORT = process.env.PORT || 3001;
-  const app = express();
+  await db;
 
   app.use(express.urlencoded({ extended: false }));
   app.use(express.json());
@@ -34,7 +41,9 @@ const startApolloServer = async () => {
     }
   ));
 
+  // if we're in production, serve client/build as static assets
   if (process.env.NODE_ENV === 'production') {
+    console.log(__dirname);
     app.use(express.static(path.join(__dirname, '../../client/dist')));
 
     app.get('*', (_req: Request, res: Response) => {
@@ -46,6 +55,10 @@ const startApolloServer = async () => {
     console.log(`API server running on port ${PORT}!`);
     console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
   });
-};
+
+}
 
 startApolloServer();
+
+//app.use(routes);
+
